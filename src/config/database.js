00ef852dbd1c4f0eeeb0f9b3cdfe4a57ca981f40
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-02-16 17:28:55 -0300

    feat: create project, server and connection database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql2',
+    host: '127.0.0.1',
+    username: 'root',
+    password: '123456',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+
+    }
+};
